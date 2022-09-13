071b05526b2965ac8ec4b304644ef2ef6355a980
Valentin Marquis <valentinm@bam.tech>
2022-09-13 14:05:54 +0200

    feat(target_platform_extension): add log when running test on unusual platform

--- lib/src/helpers/target_platform_extension.dart
+++ lib/src/helpers/target_platform_extension.dart
@@ -1,3 +1,4 @@
+import 'dart:developer';
 import 'dart:io';
 
 import 'package:flutter/foundation.dart';
@@ -5,20 +6,37 @@ import 'package:meta/meta.dart';
 
 @internal
 extension IsRuntimePlatform on TargetPlatform {
+  void _logUnusualTargetPlatformWarning() {
+    final usualTargetPlatforms = [
+      TargetPlatform.linux,
+      TargetPlatform.macOS,
+      TargetPlatform.windows
+    ];
+
+    if (usualTargetPlatforms.contains(this)) {
+      return;
+    } else {
+      log('''Tests are intended to be runned on linux, macOS or windows platform.
+      But you are running them on $name''');
+    }
+  }
+
   bool get isRuntimePlatform {
+    _logUnusualTargetPlatformWarning();
+
     switch (this) {
-      case TargetPlatform.android:
-        return Platform.isAndroid;
-      case TargetPlatform.fuchsia:
-        return Platform.isFuchsia;
-      case TargetPlatform.iOS:
-        return Platform.isIOS;
       case TargetPlatform.linux:
         return Platform.isLinux;
       case TargetPlatform.macOS:
         return Platform.isMacOS;
       case TargetPlatform.windows:
         return Platform.isWindows;
+      case TargetPlatform.android:
+        return Platform.isAndroid;
+      case TargetPlatform.fuchsia:
+        return Platform.isFuchsia;
+      case TargetPlatform.iOS:
+        return Platform.isIOS;
     }
   }
 }
