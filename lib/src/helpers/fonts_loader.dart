9a6003becc151416baf9b293755bd035b4471b97
Guillaume Diallo-Mulliez <guillaumedm@bam.tech>
2023-01-16 18:00:44 +0100

    :recycle: (fonts_loader) Add loadFontFromPackage method
    This method allows the user to load font from another package than the current one. Passing a Package object with both name and path separated allows more flexibility so as to add packages which
    name does not match the folder name

--- lib/src/helpers/fonts_loader.dart
+++ lib/src/helpers/fonts_loader.dart
@@ -8,6 +8,20 @@ import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:platform/platform.dart' as p;
 
+/// A class representing a package within a multi-packages app
+class Package {
+  /// This is the name of the package as defined in the pubspec.yaml file
+  final String? name;
+
+  /// This is the path to the package relative to where the test is run from.
+  final String? relativePath;
+
+  /// Creates a new [Package] instance.
+  /// Either [name] or [relativePath] must be provided.
+  Package({this.name, this.relativePath})
+      : assert(name != null || relativePath != null);
+}
+
 /// Load fonts to make sure they show up in golden tests.
 ///
 /// To use it efficiently:
@@ -16,8 +30,28 @@ import 'package:platform/platform.dart' as p;
 /// * add `await loadFonts();` in the `testExecutable` function.
 ///
 /// *Note* for this function to work, your package needs to include all fonts
-/// it uses in a font dir at the root of project.
+/// it uses in a font dir at the root of the project.
 Future<void> loadFonts([String? package]) async {
+  return package != null
+      ? loadFontsFromPackage(
+          package: Package(name: package, relativePath: './$package'),
+        )
+      : loadFontsFromPackage();
+}
+
+/// Load fonts from a given package to make sure they show up in golden tests.
+///
+/// To use it efficiently:
+/// * Create a flutter_test_config.dart file. See:
+/// https://api.flutter.dev/flutter/flutter_test/flutter_test-library.html
+/// * add `await loadFontsFromPackage(Package(name: 'my_theme', relativePath: './theme'));` in the `testExecutable` function.
+///
+/// *Note* for this function to work, your given package needs to include all fonts
+/// it uses in a font dir at the root of the project referenced by the given [package] argument.
+/// If no [package] is provided, it will look for a fonts dir at the root of the project.
+/// If a [package] is provided with a [Package.relativePath] it will look for a fonts dir with the package located at that path
+/// If a [package] is provided with a [Package.name] it will prefix the fonts dir with `packages/[package.name]`
+Future<void> loadFontsFromPackage({Package? package}) async {
   TestWidgetsFlutterBinding.ensureInitialized();
   await _load(loadFontsFromFontsDir(package));
   await _loadMaterialIconFont();
@@ -25,14 +59,18 @@ Future<void> loadFonts([String? package]) async {
 
 /// Assumes a fonts dir in root of project
 @visibleForTesting
-Map<String, List<Future<ByteData>>> loadFontsFromFontsDir([String? package]) {
+Map<String, List<Future<ByteData>>> loadFontsFromFontsDir([Package? package]) {
   final fontFamilyToData = <String, List<Future<ByteData>>>{};
   final currentDir = path.dirname(Platform.script.path);
   final fontsDirectory = path.join(
     currentDir,
-    package == null ? 'fonts' : '../$package/fonts',
+    package == null || package.relativePath == null
+        ? 'fonts'
+        : '${package.relativePath}/fonts',
   );
-  final prefix = package == null ? '' : 'packages/$package/';
+  final prefix = package == null || package.name == null
+      ? ''
+      : 'packages/${package.name}/';
   for (final file in Directory(fontsDirectory).listSync()) {
     if (file is File) {
       final fontFamily =
